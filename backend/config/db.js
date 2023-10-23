1c4a990e98fa1fd99d5398e9fedc96acff8b45ee
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-10-23 11:42:09 -0400

    db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,24 @@
-const { Client } = require('pg')
+const { Client } = require("pg")
 
 const client = new Client({
-	HOST: "localhost",
-	PORT: 5432,
-	database: 'postgres',
-	user: 'postgres',
-	password: 'admin123'
+  // HOST: "localhost",
+  // PORT: 5432,
+  // database: 'postgres',
+  // user: 'postgres',
+  // password: 'admin123'
+  PGHOST: process.env.DB_HOST,
+  PGPORT: process.env.DB_PORT,
+  PGDATABASE: process.env.DB_NAME,
+  PGUSER: process.env.DB_USER,
+  PGPASSWORD: process.env.DB_PASSWORD,
 })
 
 client.connect((err) => {
-	if (err) {
-		console.log('error connecting to the database');
-	} else {
-		console.log('connected');
-	}
+  if (err) {
+    console.log("error connecting to the database")
+  } else {
+    console.log("connected")
+  }
 })
 
 module.exports = client
